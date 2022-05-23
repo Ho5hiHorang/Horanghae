742e65b4add8aa18f852bbdead6613034c1edb14
Ho5hiHorang <106054594+Ho5hiHorang@users.noreply.github.com>
2022-05-23 15:13:04 +0900

    add print()
    print 기능 추가

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
+  private void print(Stirng str) {
+    System.out.println(str);
+  }
+  
   public static void main(String[] args) {
+    print("Hello world!!");
     System.out.println("Hello, World");
   }
 }
